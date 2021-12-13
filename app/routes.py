bb975373f4c27104cb85c38c269777d67f55ffba
kkkk4444 <95912155+kkkk4444@users.noreply.github.com>
2021-12-14 02:23:29 +0300

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
+from app import app
+@app.route('/')
+@app.route('/index')
+def index():
+ user = {'username': 'student '}
+ posts = [
+ {
+ 'author': {'username': 'John'},
+ 'body': 'Beautiful day in Portland!'
+ },
+ {
+ 'author': {'username': 'Susan'},
+ 'body': 'The Avengers movie was so cool!'
+ },
+ {
+ 'author': {'username': 'Василий'},
+ 'body':'Привет!!'
+ }
+ ]
+ return render_template('index.html', title='Home', user=user, posts=posts)
